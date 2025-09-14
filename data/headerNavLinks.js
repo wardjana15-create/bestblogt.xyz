1ab637bba7eda4a1906f77d0614d7bb30c0ada7a
wardjana15-create <wardjana15@gmail.com>
2025-09-14 17:14:44 +0100

    Update headerNavLinks.js

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,8 @@
 const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
+  { href: '/tags/review', title: 'Reviews' },
+  { href: '/tags/workflow', title: 'Workflows' },
+  { href: '/tags/guide', title: 'Guides' },
+  { href: '/blog', title: 'All Posts' }, // A temporary link until the Toolkit page is built
   { href: '/about', title: 'About' },
 ]
 
